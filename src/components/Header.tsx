faf41b092cb376a19f28e2e0af581a7a63c88a50
bragisivarss <sigursteinnbragi@gmail.com>
2024-05-02 14:01:22 +0000

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import hoddiogeg from "@/assets/hoddiogeg.jpg";
+import Link from "next/link";
 
-export const Header: React.FC = () => {
+export const Header = () => {
     return (
         <div className="header">
             <div className="header-link">
-                <a href="/">Home</a>
+                <Link href="/" className="header-a">Home</Link>
             </div>
             <div className="header-link">
-                <a href="./search">Search</a>
+                <Link href="./search" className="header-a">Search</Link>
             </div>
             <div className="header-link">
-                <a href="./about">About Us</a>
+                <Link href="./about" className="header-a">About Us</Link>
             </div>
             <div className="header-image">
-                <Image priority src={hoddiogeg} alt="logo" height={55} width={60} style={{borderRadius: "1rem"}}/>
+                <Image placeholder="blur" src={hoddiogeg} alt="logo" height={55} width={60} style={{borderRadius: "1rem"}}/>
             </div>
         </div>
     );
